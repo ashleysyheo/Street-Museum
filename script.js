c8aef88fe5ce7bfa90d60d5108741e24e90b7993
ashleysheo <ashleysyheo@gmail.com>
2021-10-27 14:24:01 -0400

    clean: code

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let lastPos = document.body.scrollTop || document.documentElement.scrollTop,
     (frames = $frames.toArray()),
     (message = document.getElementById("overlay"));
     numFrames = $frames.length;
-// document.body.height = (8000 * (frames.length+1))+ "px";
 
 for (var i = 0; i < numFrames; i++) {
   zVals.push((numFrames - i) * zSpacing); //array of z-values
@@ -16,17 +15,19 @@ for (var i = 0; i < numFrames; i++) {
 //function on scroll
 $(window).scroll(function (d, e) {
 
-  var top = document.body.scrollTop || document.documentElement.scrollTop,
+  let top = document.body.scrollTop || document.documentElement.scrollTop,
     delta = lastPos - top;
-  lastPos = top;
+    lastPos = top;
+
   // for every frame, translateZ it "newVal" pixels
-  for (var i = 0; i < numFrames; i++) {
-    var newZVal = (zVals[i] += delta * -1.5),
+  for (let i = 0; i < numFrames; i++) {
+    let newZVal = (zVals[i] += delta * -1.5),
       frame = frames[i],
       transform = "translateZ(" + newZVal + "px)",
       opacity = newZVal < 200 ? 1 : 1 - parseInt(((newZVal - 200) / (perspective - 200)) * 10) / 10,
       //if the newZVal is less than 200, then the opacity is one. Else, fade out.
       display = newZVal > perspective + 100  || newZVal < -1500 ? "none" : "block";
+
     //if the newZVal is greater than the perspective, make the display none, if not make the display block
     frame.setAttribute("style", "-webkit-transform:" + transform + ";-moz-transform:" + transform + ";display:" + display + ";opacity:" + opacity);
   }
@@ -34,17 +35,15 @@ $(window).scroll(function (d, e) {
 const maxRotationDegrees = 2 ;
 document.body.addEventListener("mousemove", onMove);
 document.body.addEventListener("touchmove", onMove);
-var container = document.querySelectorAll(".frame");
+let container = document.querySelectorAll(".frame");
 function onMove(e) {
-  var mousePercX = (e.pageX / document.body.clientWidth) * 2 - 1;
-  var mousePercY = (e.pageY / document.body.clientHeight) * 2 - 1;
+  let mousePercX = (e.pageX / document.body.clientWidth) * 2 - 1;
+  let mousePercY = (e.pageY / document.body.clientHeight) * 2 - 1;
 
-  var rotateX = mousePercX * maxRotationDegrees;
-  var rotateY = mousePercY * -maxRotationDegrees * 0.5;
+  let rotateX = mousePercX * maxRotationDegrees;
+  let rotateY = mousePercY * -maxRotationDegrees * 0.5;
 
-  for (var i = 0; i < container.length; i++) {
-    container[
-      i
-    ].style.transform = `translateZ(${zVals[i]}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+  for (let i = 0; i < container.length; i++) {
+    container[i].style.transform = `translateZ(${zVals[i]}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
   }
 }
